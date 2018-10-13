382049ff8908c32ec6b3444beb04cc2981a129b7
jellf2009 <jellf2009@163.com>
2018-10-13 09:38:45 +0800

    first commit

---
+++ octsct/src/main/java/com/secone/octsct/OctsctApplication.java
@@ -0,0 +1,12 @@
+package com.secone.octsct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctsctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctsctApplication.class, args);
+	}
+}
---
+++ octsct/src/test/java/com/secone/octsct/OctsctApplicationTests.java
@@ -0,0 +1,16 @@
+package com.secone.octsct;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OctsctApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
